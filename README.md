c44182662664e48fdeefaacc75e6fa7da21bfcfc
610yilingliu <yilingliu1994@gmail.com>
2020-09-07 15:25:09 +0800

    fix black problem

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# GenerateHouseNumber
-Generate house number use GAN
+# Generate House Number
+
+## Dir `raw_imgs`
+
+30 house number images from Amazon & Google Images
+
+## Dir `imgs`
+
+30 64 * 64 RGB images, generated from dir `raw_images`
+
+## Dir `gray_imgs`
+
+30 64 * 64 Gray images, the same as dir `imgs` but in gray scales
+
+## `regularize_image.py`
+
+Script to generate `imgs` and `gray_imgs`
--- regularize_image.py
+++ regularize_image.py
@@ -11,15 +11,19 @@ def regularize_img(path, dest, size):
     img = cv2.imread(path)
     h = img.shape[0]
     l = img.shape[1]
+    # If length: height is too large, it will cause deformation while resize. My solution is to put it over a white background for regularization
+    # For height:length is the same.
     if l > 1.3 * h:
         bkg_size = l
-        blank_img = np.zeros((bkg_size, bkg_size, 3), np.uint8)
+        # 255 is used to generate white background. np.zeros will generate black background. 
+        # If you still confused with it, just notice that (0, 0, 0) is black and (255, 255, 255) is white
+        blank_img = np.zeros((bkg_size, bkg_size, 3)) + 255
         start = (l - h)//2
         blank_img[start:start + h, :] = img[:, :]
         img = blank_img
     elif h > 1.3 * l:
         bkg_size = h
-        blank_img = np.zeros((bkg_size, bkg_size, 3), np.uint8)
+        blank_img = np.zeros((bkg_size, bkg_size, 3)) + 255
         start = (h - l) // 2
         blank_img[:, start:start + l] = img[:, :]
         img = blank_img
@@ -39,10 +43,10 @@ if __name__ == '__main__':
     root = './raw_imgs/'
     out = './imgs/'
 
-    # files = os.listdir(root)
-    # for f in files:
-    #     path = root + f
-    #     regularize_img(path, out + f, (64, 64))
+    files = os.listdir(root)
+    for f in files:
+        path = root + f
+        regularize_img(path, out + f, (64, 64))
 
     outgray = './img_gray/'
     files = os.listdir(out)
